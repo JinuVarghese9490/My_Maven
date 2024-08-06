d4180f41cead26ea870f032a51599644e1a2366a
JinuVarghese9490 <varghesejinu895@gmail.com>
2024-08-06 11:12:33 +0530

    Initial commit

---
+++ My_Maven/src/main/java/access/DefaultChild.java
@@ -0,0 +1,13 @@
+package access;
+
+public class DefaultChild {
+
+	public static void main(String[] args) {
+		
+		DefaultParent obj=new DefaultParent();
+		obj.msg();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ My_Maven/src/main/java/access/DefaultParent.java
@@ -0,0 +1,11 @@
+package access;
+
+public class DefaultParent 
+{
+  void msg()//not specified any access modifier it set as Default access modifier.
+  {
+	System.out.println("hello");
+  }
+
+
+}
